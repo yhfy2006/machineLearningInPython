cd2cf065e95bac9f62feaf8d0e8af328afbabdbf
Unknown <yhfy2006@gmail.com>
2016-11-01 22:42:08 -0400

    c

--- ud120-projects-master/outliers/outlier_cleaner.py
+++ ud120-projects-master/outliers/outlier_cleaner.py
@@ -10,8 +10,14 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+
+    list = []
+    for ii in range(0,len(predictions)):
+        list.append((ages[ii],net_worths[ii],[abs(net_worths[ii][0]-predictions[ii][0])]))
+
+    list.sort(key=lambda tup: tup[2], reverse=True)
     
-    cleaned_data = []
+    cleaned_data = list[10:]
 
     ### your code goes here
 
